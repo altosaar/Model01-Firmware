5b8c4732c1a49e204bfad950baa8221700ce16f9
Jaan <altosaar@users.noreply.github.com>
2018-05-08 15:56:24 -0400

    switch to dvorak; add heatmap and oneshot plugins

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -19,6 +19,13 @@
 // Support for keys that move the mouse
 #include "Kaleidoscope-MouseKeys.h"
 
+// oneshot
+#include <Kaleidoscope-OneShot.h>
+#include <kaleidoscope/hid.h>
+
+// heatmap
+#include <Kaleidoscope-Heatmap.h>
+
 // Support for macros
 #include "Kaleidoscope-Macros.h"
 
@@ -121,7 +128,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { QWERTY, NUMPAD, FUNCTION }; // layers
+enum { DVORAK, NUMPAD, FUNCTION }; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -130,20 +137,20 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
 
 KEYMAPS(
 
-  [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+  [DVORAK] = KEYMAP_STACKED
+  (___,          Key_1,         Key_2,     Key_3,      Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick, Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, Key_Tab,
+   Key_PageUp,   Key_A,         Key_O,     Key_E,      Key_U, Key_I,
+   Key_PageDown, Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_Escape,
+   OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
+   OSL(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+   M(MACRO_ANY),   Key_6, Key_7, Key_8, Key_9, Key_0, LockLayer(NUMPAD),
+   Key_Enter,      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
+                   Key_D, Key_H, Key_T, Key_N, Key_S, Key_Minus,
+   Key_RightAlt,   Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
+   OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightControl),
+   OSL(FUNCTION)),
 
 
   [NUMPAD] =  KEYMAP_STACKED
@@ -304,6 +311,12 @@ void setup() {
     // We start with the LED effect that turns off all the LEDs.
     &LEDOff,
 
+    // oneshot
+    &OneShot,
+
+    // heatmap
+    &HeatmapEffect,
+
     // The rainbow effect changes the color of all of the keyboard's keys at the same time
     // running through all the colors of the rainbow.
     &LEDRainbowEffect,
@@ -343,6 +356,9 @@ void setup() {
     // and allows us to turn LEDs off when it goes to sleep.
     &HostPowerManagement
   );
+
+  // heatmap
+  HeatmapEffect.activate();
 
   // While we hope to improve this in the future, the NumPad plugin
   // needs to be explicitly told which keymap layer is your numpad layer
