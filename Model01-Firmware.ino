8a8139c87b1fec85edcd5ae50fba2be33764521b
Jaan <altosaar@users.noreply.github.com>
2018-10-08 10:52:41 -0400

    substitute right butterfly key for right gui key

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -190,7 +190,7 @@ KEYMAPS(
    M(MACRO_ANY),   Key_6, Key_7, Key_8, Key_9, Key_0, LockLayer(NUMPAD),
    Key_Enter,      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
                    Key_D, Key_H, Key_T, Key_N, Key_S, Key_Minus,
-   Key_RightAlt,   Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
+   OSM(RightGui),  Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
    OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightControl),
    OSL(FUNCTION)),
 
